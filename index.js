a9966dd86b903e31958f8e0082a66aa0d338c5a5
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-12-04 18:50:56 -0500

    [ADD]Landpage done without grid style change

---
+++ index.js
@@ -0,0 +1,50 @@
+import { data } from './mock.js';
+
+const galleryContainer = document.getElementById('galleryContainer');
+
+const dataAll = document.getElementsByClassName('dataAll');
+const dataBrand = document.getElementsByClassName('dataBrand');
+const dataWeb = document.getElementsByClassName('dataWeb');
+const dataPhoto = document.getElementsByClassName('dataPhoto');
+const dataMobile = document.getElementsByClassName('dataMobile');
+
+let all = [];
+
+function allPortfolio(category) {
+  for(let i = 0; i < category.length - 1 ; i++){
+    all.push(category[i])
+  }
+}
+
+allPortfolio(data.brand)
+allPortfolio(data.web)
+allPortfolio(data.mobile)
+
+const displayItems = ((items) => {
+  let itemsArray = '';
+  items.forEach((item) => {
+    itemsArray += `
+      <div class="img__container">
+        <img class="img__portfolio" src="./assets/${item.img}" alt="">
+        <div class="overlay">
+          <h2> ${item.title} </h2>
+          <p>${item.description}</p>
+        </div>
+      </div>
+    `
+  });
+  galleryContainer.innerHTML = itemsArray;
+});
+
+dataAll[0].addEventListener('click', function() { displayItems(all)}, false );
+dataAll[1].addEventListener('click', function() { displayItems(all)}, false );
+dataWeb[0].addEventListener('click', function() { displayItems(data.web)}, false );
+dataWeb[1].addEventListener('click', function() { displayItems(data.web)}, false );
+dataBrand[0].addEventListener('click', function() { displayItems(data.brand)}, false );
+dataBrand[1].addEventListener('click', function() { displayItems(data.brand)}, false );
+dataPhoto[0].addEventListener('click', function() { displayItems(data.photo)}, false );
+dataPhoto[1].addEventListener('click', function() { displayItems(data.photo)}, false );
+dataMobile[0].addEventListener('click', function() { displayItems(data.mobile)}, false );
+dataMobile[1].addEventListener('click', function() { displayItems(data.mobile)}, false );
+
+displayItems(all)
---
+++ mock.js
@@ -0,0 +1,192 @@
+export const data = {
+  brand:[
+    {
+      title: 'Lorem Ipsum',
+      description: 'Branding',
+      img: 'brand/brand-1.jpg'
+    },
+    {
+      title: 'Suffered Alteration',
+      description: 'Branding',
+      img: 'brand/brand-2.jpg'
+    },
+    {
+      title: 'Will Uncover',
+      description: 'Branding',
+      img: 'brand/brand-3.jpg'
+    },
+    {
+      title: 'Lorem Ipsum',
+      description: 'Branding',
+      img: 'brand/brand-4.jpg'
+    },
+    {
+      title: 'Suffered Alteration',
+      description: 'Branding',
+      img: 'brand/brand-5.jpg'
+    },
+    {
+      title: 'Will Uncover',
+      description: 'Branding',
+      img: 'brand/brand-6.jpg'
+    },
+    {
+      title: 'Lorem Ipsum',
+      description: 'Branding',
+      img: 'brand/brand-7.jpg'
+    },
+    {
+      title: 'Suffered Alteration',
+      description: 'Branding',
+      img: 'brand/brand-8.jpg'
+    },
+    {
+      title: 'Will Uncover',
+      description: 'Branding',
+      img: 'brand/brand-9.jpg'
+    },
+  ],
+  web:[
+    {
+      title: 'Lorem Ipsum',
+      description: 'Web',
+      img: 'web/web-1.jpg'
+    },
+    {
+      title: 'Suffered Alteration',
+      description: 'Web',
+      img: 'web/web-2.jpg'
+    },
+    {
+      title: 'Will Uncover',
+      description: 'Web',
+      img: 'web/web-3.jpg'
+    },
+    {
+      title: 'Lorem Ipsum',
+      description: 'Web',
+      img: 'web/web-4.jpg'
+    },
+    {
+      title: 'Suffered Alteration',
+      description: 'Web',
+      img: 'web/web-5.jpg'
+    },
+    {
+      title: 'Will Uncover',
+      description: 'Web',
+      img: 'web/web-6.jpg'
+    },
+    {
+      title: 'Lorem Ipsum',
+      description: 'Web',
+      img: 'web/web-7.jpg'
+    },
+    {
+      title: 'Suffered Alteration',
+      description: 'Web',
+      img: 'web/web-8.jpg'
+    },
+    {
+      title: 'Will Uncover',
+      description: 'Web',
+      img: 'web/web-9.jpg'
+    },
+  ],
+  photo:[
+    {
+      title: 'Lorem Ipsum',
+      description: 'Photography',
+      img: 'photo/photo-1.jpg'
+    },
+    {
+      title: 'Suffered Alteration',
+      description: 'Photography',
+      img: 'photo/photo-2.jpg'
+    },
+    {
+      title: 'Will Uncover',
+      description: 'Photography',
+      img: 'photo/photo-3.jpg'
+    },
+    {
+      title: 'Lorem Ipsum',
+      description: 'Photography',
+      img: 'photo/photo-4.jpg'
+    },
+    {
+      title: 'Suffered Alteration',
+      description: 'Photography',
+      img: 'photo/photo-5.jpg'
+    },
+    {
+      title: 'Will Uncover',
+      description: 'Photography',
+      img: 'photo/photo-6.jpg'
+    },
+    {
+      title: 'Lorem Ipsum',
+      description: 'Photography',
+      img: 'photo/photo-7.jpg'
+    },
+    {
+      title: 'Suffered Alteration',
+      description: 'Photography',
+      img: 'photo/photo-8.jpg'
+    },
+    {
+      title: 'Will Uncover',
+      description: 'Photography',
+      img: 'photo/photo-9.jpg'
+    },
+  ],
+  mobile:[
+    {
+      title: 'Lorem Ipsum',
+      description: 'App',
+      img: 'mobile/mobile-1.jpg'
+    },
+    {
+      title: 'Suffered Alteration',
+      description: 'App',
+      img: 'mobile/mobile-2.jpg'
+    },
+    {
+      title: 'Will Uncover',
+      description: 'App',
+      img: 'mobile/mobile-3.jpg'
+    },
+    {
+      title: 'Lorem Ipsum',
+      description: 'App',
+      img: 'mobile/mobile-4.jpg'
+    },
+    {
+      title: 'Suffered Alteration',
+      description: 'App',
+      img: 'mobile/mobile-5.jpg'
+    },
+    {
+      title: 'Will Uncover',
+      description: 'App',
+      img: 'mobile/mobile-6.jpg'
+    },
+    {
+      title: 'Lorem Ipsum',
+      description: 'App',
+      img: 'mobile/mobile-7.jpg'
+    },
+    {
+      title: 'Suffered Alteration',
+      description: 'App',
+      img: 'mobile/mobile-8.jpg'
+    },
+    {
+      title: 'Will Uncover',
+      description: 'App',
+      img: 'mobile/mobile-9.jpg'
+    },
+  ],
+}
+
+
